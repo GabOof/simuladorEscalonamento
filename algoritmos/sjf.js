0ab676cc6cfb41da938cc031ac98ac66bf84b01f
Gabrielle Fonseca <oliveirafonseca.gab@gmail.com>
2025-11-10 11:30:06 -0300

    Implementa ordenação por burst time e cálculo dos tempos no SJF

--- algoritmos/sjf.js
+++ algoritmos/sjf.js
@@ -6,5 +6,36 @@ function simularSJF(pacientes, medicos) {
 
   pacientes.sort((a, b) => a.chegada - b.chegada);
 
-  console.log("SJF iniciado com", pacientes.length, "pacientes");
+  let tempoAtual = 0;
+  let filaProntos = [];
+  let ordemExecucao = [];
+  let totalEspera = 0;
+  let totalTurnaround = 0;
+
+  while (pacientes.length > 0 || filaProntos.length > 0) {
+    while (pacientes.length > 0 && pacientes[0].chegada <= tempoAtual) {
+      filaProntos.push(pacientes.shift());
+    }
+
+    if (filaProntos.length === 0) {
+      tempoAtual++;
+      continue;
+    }
+
+    filaProntos.sort((a, b) => a.duracao - b.duracao);
+    const atual = filaProntos.shift();
+
+    const inicio = tempoAtual;
+    const fim = inicio + atual.duracao;
+    const espera = inicio - atual.chegada;
+    const turnaround = fim - atual.chegada;
+
+    totalEspera += espera;
+    totalTurnaround += turnaround;
+    tempoAtual = fim;
+
+    ordemExecucao.push({ nome: atual.nome, inicio, fim });
+  }
+
+  console.log("Execução SJF concluída:", ordemExecucao);
 }
